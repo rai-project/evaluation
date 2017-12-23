ba50ac9fe79ca86a588ba7616c1f07305c5bfa13
Abdul Dakkak <abduld@wolfram.com>
2017-12-23 09:31:40 -0600

    add overwrite version

--- cmd/run_all.sh
+++ cmd/run_all.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #go build main.go
-./main latency --model_name all --overwrite -f json --database_name carml_step_trace &
-./main layers --model_name all --overwrite -f json --database_name carml_full_trace &
-./main layer_tree --model_name all --overwrite -f json --database_name carml_full_trace &
-./main cuda_launch --model_name all --overwrite -f json --database_name carml_full_trace &
-./main eventflow --model_name all --overwrite -f json --database_name carml_full_trace &
+./main latency --model_name all -f json --database_name carml_step_trace &
+./main layers --model_name all -f json --database_name carml_full_trace &
+./main layer_tree --model_name all -f json --database_name carml_full_trace &
+./main cuda_launch --model_name all -f json --database_name carml_full_trace &
+./main eventflow --model_name all -f json --database_name carml_full_trace &
 wait
 
