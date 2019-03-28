61ff05718bdbbf9471b8a0019a52e6d9fee422d8
Abdul Dakkak <abduld@wolfram.com>
2019-03-28 00:18:58 -0500

    Work

---
+++ metrics/perceptual_quality.go
@@ -0,0 +1,6 @@
+package metrics
+
+// https://www.pirm2018.org/PIRM-SR.html
+// \text{Perceptual index} = \tfrac{1}{2} ((10 - \text{Ma}) + \text{NIQE}).
+// https://sites.google.com/site/chaoma99/sr-metric
+// https://ieeexplore.ieee.org/document/6353522/
