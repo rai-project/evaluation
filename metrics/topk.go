493d63ac95ea982ce552b249f1bf8fcb328912c7
Abdul Dakkak <abduld@wolfram.com>
2019-03-31 16:27:19 -0500

    refactor

--- metrics/topk.go
+++ metrics/topk.go
@@ -23,3 +23,25 @@ func ClassificationTop1(features *dlframework.Features, expectedLabelIndex int) 
 func Top1(features *dlframework.Features, expectedLabelIndex int) bool {
 	return ClassificationTop1(features, expectedLabelIndex)
 }
+
+func ClassificationTop5(features *dlframework.Features, expectedLabelIndex int) bool {
+	for _, feature := range []*dlframework.Feature(*features) {
+		_, ok := feature.Feature.(*dlframework.Feature_Classification)
+		if !ok {
+			panic("unable to convert first feature to boundingbox")
+		}
+	}
+
+	sort.Sort(features)
+
+	for _, feature := range []*dlframework.Feature(*features)[:5] {
+		if int(feature.Feature.(*dlframework.Feature_Classification).Classification.Index) == expectedLabelIndex {
+			return true
+		}
+	}
+	return false
+}
+
+func Top5(features *dlframework.Features, expectedLabelIndex int) bool {
+	return ClassificationTop5(features, expectedLabelIndex)
+}
